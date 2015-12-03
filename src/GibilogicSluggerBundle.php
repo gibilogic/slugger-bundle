19a3830b5c56a3a020e45b1f4d10ccd6779a86f6
Matteo Guindani <m.guindani@gibilogic.com>
2015-12-03 11:58:55 +0100

    Added the 'GibilogicSluggerBundle' main bundle class

---
+++ src/GibilogicSluggerBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @package     Gibilogic\SluggerBundle
+ * @author      GiBiLogic <info@gibilogic.com>
+ * @authorUrl   http://www.gibilogic.com
+ */
+
+namespace Gibilogic\SluggerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * GibilogicSluggerBundle class.
+ *
+ * @see \Symfony\Component\HttpKernel\Bundle\Bundle
+ */
+class GibilogicSluggerBundle extends Bundle
+{
+
+}
